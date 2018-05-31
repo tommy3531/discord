754e90640213fc49846455fda5f161948bf1d306
Tom Marler <thomas.marler@hotmail.com>
2018-05-31 14:35:44 -0500

    Pong and Name Bot

---
+++ pong.js
@@ -0,0 +1,36 @@
+// const express        = require('express');
+// const MongoClient    = require('mongodb').MongoClient;
+// const bodyParser     = require('body-parser');
+// const app            = express();
+// const port = 8000;
+// app.use(bodyParser.urlencoded({ extended: true }));
+// // Connection URL
+// var url = 'mongodb://tom:!Tommy3531@ds121599.mlab.com:21599/rails_political_data';
+
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, database) {
+// 	var railsDB = database.db('rails_political_data');
+// 	var politicanCollection = railsDB.collection('politican');
+// 	console.log(politicanCollection);
+// });
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+
+  if (message.content.startsWith("Tom")) {
+  	message.channel.send("Your Awesome!!!");
+  }
+
+  // Returns @iOSTom
+  if (message.content == 'what is my avatar') {
+  	message.reply(this.avatorURL);
+  }
+});
+
+client.login("NDUxNTc0ODQzMzg1NzA4NTU0.DfD-cw.S0pt-Dhbs4p1SiLmpi3MRjH9O3U");
---
+++ watcher.js
@@ -0,0 +1,26 @@
+// const express        = require('express');
+// const MongoClient    = require('mongodb').MongoClient;
+// const bodyParser     = require('body-parser');
+// const app            = express();
+// const port = 8000;
+// app.use(bodyParser.urlencoded({ extended: true }));
+// // Connection URL
+// var url = 'mongodb://tom:!Tommy3531@ds121599.mlab.com:21599/rails_political_data';
+
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, database) {
+// 	var railsDB = database.db('rails_political_data');
+// 	var politicanCollection = railsDB.collection('politican');
+// 	console.log(politicanCollection);
+// });
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+client.login("NDUxNTc0ODQzMzg1NzA4NTU0.DfD-cw.S0pt-Dhbs4p1SiLmpi3MRjH9O3U");
